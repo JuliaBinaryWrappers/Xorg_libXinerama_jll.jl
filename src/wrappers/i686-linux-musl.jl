81b7f1ac55739ac6ddaf452cfee4ebd9eeead199
jlbuild <juliabuildbot@gmail.com>
2020-10-16 19:01:41 +0000

    Xorg_libXinerama_jll build 1.1.4+4

--- src/wrappers/i686-linux-musl.jl
+++ src/wrappers/i686-linux-musl.jl
@@ -2,52 +2,15 @@
 export libXinerama
 
 using Xorg_libXext_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "LD_LIBRARY_PATH"
-LIBPATH_default = ""
-
-# Relative path to `libXinerama`
-const libXinerama_splitpath = ["lib", "libXinerama.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libXinerama_path = ""
-
-# libXinerama-specific global declaration
-# This will be filled out by __init__()
-libXinerama_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libXinerama = "libXinerama.so.1"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("Xorg_libXinerama")
+JLLWrappers.@declare_library_product(libXinerama, "libXinerama.so.1")
 function __init__()
-    global artifact_dir = abspath(artifact"Xorg_libXinerama")
+    JLLWrappers.@generate_init_header(Xorg_libXext_jll)
+    JLLWrappers.@init_library_product(
+        libXinerama,
+        "lib/libXinerama.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (Xorg_libXext_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (Xorg_libXext_jll.LIBPATH_list,))
-
-    global libXinerama_path = normpath(joinpath(artifact_dir, libXinerama_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libXinerama_handle = dlopen(libXinerama_path)
-    push!(LIBPATH_list, dirname(libXinerama_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ':')
-    global LIBPATH = join(vcat(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ':')
-
-    
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
