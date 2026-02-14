5428172f8b70714cd862317f637acee5758c5469
jlbuild <juliabuildbot@gmail.com>
2026-02-14 19:51:43 +0000

    Xorg_libXinerama_jll build 1.1.7+0

--- src/Xorg_libXinerama_jll.jl
+++ src/Xorg_libXinerama_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Xorg_libXinerama")
-JLLWrappers.@generate_main_file("Xorg_libXinerama", UUID("d1454406-59df-5ea1-beac-c340f2130bc3"))
+JLLWrappers.@generate_main_file("Xorg_libXinerama", Base.UUID("d1454406-59df-5ea1-beac-c340f2130bc3"))
 end  # module Xorg_libXinerama_jll
